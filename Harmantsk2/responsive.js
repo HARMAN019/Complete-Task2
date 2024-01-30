51e2336169b3d6773d72e63382ecbca67687eb5b
harmanpreet singh <84337262+HARMAN019@users.noreply.github.com>
2024-01-30 17:55:48 +0530

    Add files via upload

---
+++ Harmantsk2/responsive.js
@@ -0,0 +1,7 @@
+$('.control-line').click(function(){
+    $('.line1').toggleClass("line1-transform");
+    $('.line2').toggleClass("line2-transform");
+    $('.line3').toggleClass("line3-transform");
+    $('.toggle-button').toggleClass("toggle-active");
+})
+
